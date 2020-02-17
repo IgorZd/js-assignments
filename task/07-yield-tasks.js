d02660bd358f9878e7bd6a3dbe112d74dabe79b7
Ihar Zdanevich <Ihar_Zdanevich@epam.com>
2020-02-17 17:14:09 +0300

    chapter 08  is done

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable max-len */
 /** ******************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
@@ -31,8 +31,16 @@
  * @return {Iterable.<string>}
  *
  */
-function get99BottlesOfBeer() {
-  throw new Error('Not implemented');
+function* get99BottlesOfBeer() {
+  for (let i = 99; i > 1;) {
+    yield `${i} bottles of beer on the wall, ${i--} bottles of beer.`;
+    yield `Take one down and pass it around, ${i} bottle${i > 1 ? 's' : ''} of beer on the wall.`;
+  }
+
+  yield '1 bottle of beer on the wall, 1 bottle of beer.';
+  yield 'Take one down and pass it around, no more bottles of beer on the wall.';
+  yield 'No more bottles of beer on the wall, no more bottles of beer.';
+  yield 'Go to the store and buy some more, 99 bottles of beer on the wall.';
 }
 
 
